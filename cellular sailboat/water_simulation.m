dd4ce1fa18ff312f064374309ce5885d8166bf2c
Francisco Sebastiano <137376492+franciscoSebastiano@users.noreply.github.com>
2023-07-14 20:40:11 -0400

    Update water_simulation.m
    Fixed some formatting and removed unnecessary pieces of code leftover from a different project

--- cellular sailboat/water_simulation.m
+++ cellular sailboat/water_simulation.m
@@ -1,24 +1,12 @@
 clc, clearvars, clf
 
-
 size_of_game = 50;
 mylife = randi([0 1],size_of_game,size_of_game);
 newlife = mylife;
 
-%chat gpt wrote the below section
-% Create the object (CHAT GPT SECTION START)
-bobbing_object_position = [size_of_game/2, size_of_game/2];  % Change these values to set the position of the object
-bobbing_object_amplitude = 5;  % The amount the object bobs up and down
-bobbing_object_frequency = 1;  % How quickly the object bobs up and down
-bobbing_object_phase = 0;  % The starting point in the bobbing cycle
-bobbing_object_time = 0;  % Keeps track of the time for the sine function
-% Create the object (CHAT GPT SECTION END)
-
 figure;
 
-
 while 1
-
 
 for i = 1:size_of_game
     si = i-1;
@@ -59,11 +47,6 @@ end
     
 mylife = newlife;
 pause(0.01);
-
-bobbing_object_time = bobbing_object_time + 1;
-bobbing_object_height = bobbing_object_amplitude * sin(bobbing_object_frequency * bobbing_object_time + bobbing_object_phase);
-bobbing_object_height = bobbing_object_height + bobbing_object_amplitude;
-
 
 surf(mylife); 
 hold on;  
